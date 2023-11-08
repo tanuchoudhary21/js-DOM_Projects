04c4423ccf6c98b02554fe15ae61c9b4403e6de8
Tanu Choudhary <chtaruna90@gmail.com>
2023-11-09 02:48:38 +0530

    Added Add Friend feature using single button

--- FB-Friend_Request/script.js
+++ FB-Friend_Request/script.js
@@ -1,13 +1,24 @@
 var istatus = document.querySelector("h5");
 var addFriend = document.querySelector("#add");
-var removeFriend = document.querySelector("#remove");
-
+// var removeFriend = document.querySelector("#remove");
+var flag = 0;
 addFriend.addEventListener("click", function(){
-    istatus.innerHTML = "Friends";
-    istatus.style.color = "green"
+    if(flag == 0){
+        istatus.innerHTML = "Friends";
+        istatus.style.color = "green";
+        addFriend.innerHTML = "Remove";
+        flag = 1;
+    }else{
+        istatus.innerHTML = "Stranger";
+        istatus.style.color = "red";
+        addFriend.innerHTML = "Add Friend";
+        flag = 0;
+    }
+    
+
 })
 
-removeFriend.addEventListener("click", function(){
-    istatus.innerHTML = "Stranger";
-    istatus.style.color = "red";
-})
+// removeFriend.addEventListener("click", function(){
+//     istatus.innerHTML = "Stranger";
+//     istatus.style.color = "red";
+// })
